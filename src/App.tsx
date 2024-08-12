8d97b2f76dec6caa8a02ba1b64762cdfa8e106b8
Joseph Wyman <jwymandev@gmail.com>
2024-08-12 18:04:21 -0400

    use createHashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,45 @@ import { createTheme, MantineProvider } from "@mantine/core";
 import { Home } from "./pages/Home";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  Routes,
+  Route,
+  createHashRouter,
+  RouterProvider,
+  Outlet,
+} from "react-router-dom";
 import { Header } from "./pages/Header";
 import { Download } from "./pages/Download";
+const Layout = () => {
+  return (
+    <div>
+      <Header />
+      <main>
+        <Outlet /> {/* This is where the route components will be rendered */}
+      </main>
+    </div>
+  );
+};
 
 const queryClient = new QueryClient();
+
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <Layout />, // Use Layout here to wrap around routes
+    children: [
+      {
+        path: "/", // Home route
+        element: <Home />,
+      },
+      {
+        path: "download", // Download route
+        element: <Download />,
+      },
+      // Add more routes here as needed
+    ],
+  },
+]);
 
 const theme = createTheme({
   /** Put your mantine theme override here */
@@ -22,13 +56,7 @@ function Main() {
   return (
     <MantineProvider theme={theme}>
       <QueryClientProvider client={queryClient}>
-        <Router basename={process.env.PUBLIC_URL}>
-          <Header />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/download" element={<Download />} />
-          </Routes>
-        </Router>
+        <RouterProvider router={router}></RouterProvider>
       </QueryClientProvider>
     </MantineProvider>
   );
