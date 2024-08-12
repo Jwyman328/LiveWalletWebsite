920422239b75d0a739989b1e73e49cc27e5cffb8
Joseph Wyman <jwymandev@gmail.com>
2024-08-12 17:46:27 -0400

    use hash router

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import { createTheme, MantineProvider } from "@mantine/core";
 import { Home } from "./pages/Home";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { Header } from "./pages/Header";
 import { Download } from "./pages/Download";
 
